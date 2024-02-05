a45ff3239a7a2c38337f16ac9b5a11de6741d644
noorh22 <noorulhuda987@gmail.com>
2024-02-05 09:33:45 +0500

    topic 1 complete

---
+++ README.md
@@ -0,0 +1,7 @@
+### Introduction to Computing for Data Analysis (CSE-6040) - Spring 2024
+
+*Author: Noor ul Huda* <br>
+
+The repository contains code for notebooks, assignments, exam preparation or general practice problems respective to the course. 
+This repository is sole work of the author and the code written is only intended for educational purposes.
+
--- module0/topic1.py
+++ module0/topic1.py
@@ -1,9 +1,9 @@
-
 # PART 0
 def strcat_ba(a, b):
     assert type(a) is str, f"Input argument `a` has `type(a)` is {type(a)} rather than `str`"
     assert type(b) is str, f"Input argument `b` has `type(b)` is {type(b)} rather than `str`"
     return b + a
+
 
 def strcat_list(L):
     assert type(L) is list
@@ -21,21 +21,24 @@ def floor_fraction(a, b):
     assert is_number(b) and b > 0
     return int(a // b)
 
+
 def ceiling_fraction(a, b):
     assert is_number(a) and a >= 0
     assert is_number(b) and b > 0
-    return int(a//b + 1)
+    return int(a // b + 1)
 
 
 def report_exam_avg(a, b, c):
     avg = (a + b + c) / 3
-    return 'Your average score: ' + str(round(avg , 1))
+    return 'Your average score: ' + str(round(avg, 1))
+
 
 def count_word_lengths(s):
     assert all([x.isalpha() or x == ' ' for x in s])
     assert type(s) is str
     res = [len(x) for x in s.split()]
     return res
+
 
 # PART 1
 
@@ -49,9 +52,10 @@ def remove_all(L, x):
     remove_list = [i for i in L if i != x]
     return remove_list
 
+
 def compress_vector(x):
     assert type(x) is list
-    d = {'inds': [j for j in range(len(x)) if x[j] != 0],'vals': [i for i in x if i != 0]}
+    d = {'inds': [j for j in range(len(x)) if x[j] != 0], 'vals': [i for i in x if i != 0]}
     print(d)
     return d
 
@@ -92,9 +96,57 @@ def find_common_inds(d1, d2):
 
     return array
 
+
 # PART 2
+def get_students(grades):
+    names_list = [grades[i][0] for i in range(1, len(grades))]
+    return names_list
 
 
+def get_assignments(grades):
+    assignments_list = [grades[0][i] for i in range(1, len(grades[0]))]
+    return assignments_list
 
 
+# Create a dict mapping names to lists of grades.
+def build_grade_lists(grades):
+    dict = {}
+    for i in range(1, len(grades)):
+        dict[grades[i][0]] = [int(grades[i][j]) for j in range(1, len(grades[i]))]
+    return dict
 
+
+def build_grade_dicts(grades):
+    grades_dict = {grades[i][0]: {grades[0][j]: int(grades[i][j]) for j in range(1, len(grades[i]))} for i in
+                   range(1, len(grades))}
+    return grades_dict
+
+
+# Create a dict mapping names to grade averages.
+
+
+def build_avg_by_student(grades):
+    avg_dict = {grades[i][0]: statistics.mean(int(j) for j in grades[i][1:]) for i in range(1, len(grades))}
+    return avg_dict
+
+
+def build_grade_by_asn(grades):
+    asn_dict = {grades[0][i]: [int(grades[j][i]) for j in range(1, len(grades))] for i in range(1, len(grades[0]))}
+    return asn_dict
+
+
+# Create a dict mapping items to average for that item across all students.
+import statistics
+
+
+def build_avg_by_asn(grades):
+    asn_avg_dict = {grades[0][i]: statistics.mean([int(grades[j][i]) for j in range(1, len(grades))]) for i in
+                    range(1, len(grades[0]))}
+    return asn_avg_dict
+
+
+def get_ranked_students(grades):
+    names = [grades[i][0] for i in range(1, len(grades))]
+    avg_scores = [statistics.mean(int(j) for j in grades[i][1:]) for i in range(1, len(grades))]
+    names_list = [j[1] for j in sorted(zip(avg_scores, names), reverse=True)]
+    return names_list
