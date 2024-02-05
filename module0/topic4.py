9b878edc15fd6f5521fe5e9380f9cbca763f0a0d
noorh22 <noorulhuda987@gmail.com>
2024-02-05 09:12:45 +0500

    intial commit

---
+++ module0/topic4.py
@@ -0,0 +1,16 @@
+# NOTEBOOK 4
+# PART 0
+
+# PART 1
+N = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
+# Initialize an array t of size len(N) to all zeroes.
+t = [0.0] * len(N)
+
+# Your code should do the experiment described above for
+# each problem size N[i], and store the computed sum in t[i].
+
+###
+### YOUR CODE HERE
+###
+
+print(t)
